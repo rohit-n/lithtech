734e491f8d543e56de7479e5d683772496cde853
René Kjellerup <rk.katana.steel@gmail.com>
2017-06-06 17:50:28 -0700

    fixed a few case sensitive paths for cmake.
    fixed when not initializing baseclass prior to member variables.
    
    setup allocated pointer math with a pointer size agnostic interger
    type instead of only using 32bit integer.

--- libs/stdlith/l_allocator.cpp
+++ libs/stdlith/l_allocator.cpp
@@ -1,6 +1,8 @@
 
 #include "l_allocator.h"
+#include <cstdint>
 
+using std::uintptr_t;
 
 LAlloc g_DefAlloc;
 
@@ -122,7 +124,7 @@ void* LAllocSimpleBlock::Alloc(uint32 size, bool bQuadWordAlign)
 
 	if (bQuadWordAlign) {	// QuadWord Align (We've over alloced a bit to account for this - if we're not QWAligned, force it to be)...
 		pRet = &m_pBlock[m_CurBlockPos];
-		m_CurBlockPos += (((uint32)pRet + 0xf) & ~0xf) - (uint32)pRet; }
+		m_CurBlockPos += (((uintptr_t)pRet + 0xf) & ~0xf) - (uintptr_t)pRet; }
 	else {					// DWord Align by default...
 		size = ((size + 3) & ~3); }
 
@@ -131,7 +133,7 @@ void* LAllocSimpleBlock::Alloc(uint32 size, bool bQuadWordAlign)
 
 	pRet = &m_pBlock[m_CurBlockPos];
 	m_CurBlockPos += size;
-	assert(!bQuadWordAlign || ((uint32)pRet & 0xf) == 0);
+	assert(!bQuadWordAlign || ((uintptr_t)pRet & 0xf) == 0);
 	return pRet;
 }
 
