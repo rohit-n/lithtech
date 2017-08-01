29b4b5384e67cb2c7926118f78d6449d7899d707
Rene Kjellerup <rk.katana.steel@gmail.com>
2017-08-01 14:55:02 -0700

    added a small email about the client build

--- tests/validate.py
+++ tests/validate.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import sys
+import smtplib
+from email.mime.text import MIMEText
 
 logs = []
 with open(sys.argv[1], 'r') as log:
@@ -18,7 +20,10 @@ mins = {
     'warning': int(sys.argv[4]),
     'undefined reference': int(sys.argv[5])
 }
+client = []
 for l in logs:
+    if 'EXE_Lithtech' in l:
+        client.append(l[:-1])
     for k in counts.keys():
         if k in l:
             counts[k] += 1
@@ -26,6 +31,15 @@ for l in logs:
 fmt = 'Pass:  {Pass}\nFail:  {Fail}\n'
 fmt += 'Warn:  {warning}\nUnDef: {undefined reference}'
 print(fmt.format(**counts))
+
+mime = MIMEText('\n'.join(client))
+mime['Subject'] = 'Lithtech client build - Travis'
+mime['From'] = 'no-reply@example.com'
+mime['To'] = 'rk.katana.steel@gmail.com'
+
+s = smtplib.SMTP('localhost')
+s.send_message(mime)
+s.quit()
 
 if mins['Pass'] > counts['Pass']:
     print('component passes has gone down')
@@ -35,3 +49,5 @@ for k in ['Fail', 'warning', 'undefined reference']:
     if mins[k] < counts[k]:
         print('{0} parts have gone above known limits'.format(k))
         sys.exit(1)
+
+
