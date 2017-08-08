962387395c42df14920c3cfa633b968776899e49
Rene Kjellerup <rk.katana.steel@gmail.com>
2017-08-08 15:12:12 -0700

    fixed vtable for CTextureStringImage collection stub

--- runtime/client/src/sys/linux/texturestringimage.cpp
+++ runtime/client/src/sys/linux/texturestringimage.cpp
@@ -3,11 +3,15 @@
 
 
 CTextureStringImage::CTextureStringImage() :
+    ILTRefCount{},
 	m_pGlyphList{nullptr},
 	m_nNumGlyphs{0},
 	m_nRowHeight{0}
 {
 }
+
+CTextureStringImage::~CTextureStringImage() 
+{}
 
 const CTextureStringGlyph* CTextureStringImage::GetGlyph(char cGlyph) const
 {
@@ -18,8 +22,17 @@ CTextureStringImage* CTextureStringImage::Allocate()
 {
     return nullptr;
 }
+void CTextureStringImage::Free(CTextureStringImage* img)
+{}
 
 bool CTextureStringImage::CreateBitmapFont(const char*, const CFontInfo&)
+{
+    return false;
+}
+void CTextureStringImage::FreeData()
+{}
+
+bool CTextureStringImage::SetupUniqueGlyphList(const char* pszString)
 {
     return false;
 }
