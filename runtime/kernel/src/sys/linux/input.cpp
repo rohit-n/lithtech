45d32b466447023f6f839994c0971641b251b166
Rene Kjellerup <rk.katana.steel@gmail.com>
2017-08-10 12:58:53 -0700

    5: supplied Term function to InputMgr

--- runtime/kernel/src/sys/linux/input.cpp
+++ runtime/kernel/src/sys/linux/input.cpp
@@ -1,9 +1,14 @@
 #include "bdefs.h"
 #include "input.h"
 
+void in_Term(InputMgr*)
+{
+}
+
 LTRESULT input_GetManager(InputMgr **pMgr)
 {
     static InputMgr mgr{};
+    mgr.Term = &in_Term;
     *pMgr = &mgr;
     return LT_OK;
 }
