067b7568f0250501a0f302fbf59e2b31e35b5775
Rene Kjellerup <rk.katana.steel@gmail.com>
2018-02-27 14:18:07 -0800

    adding my custom load string definition to serverDir lib and linking to Client shell and res

--- libs/DynRes/dynres.cpp
+++ libs/DynRes/dynres.cpp
@@ -4,9 +4,10 @@
 
 typedef const char* (*resString)(uint32_t);
 
-void LoadString(void *h, int id, char* buff, int len)
+uint32_t LoadString(void *h, int id, char* buff, int len)
 {
     auto s = (resString*)dlsym(h, "LoadString");
     const char *tmp =  (*s)(id);
     std::strncpy(buff, tmp, len);
+    return std::strlen(buff);
 }
--- libs/DynRes/dynres.h
+++ libs/DynRes/dynres.h
@@ -1,6 +1,7 @@
 #ifndef __lib_dynres_h__
 #define __lib_dynres_h__
-void LoadString(void *h, int id, char* buff, int len);
+#include <cinttypes>
+uint32_t LoadString(void *h, int id, char* buff, int len);
 
 void setup_string_tables(void);
 void setup_cursors(void);
