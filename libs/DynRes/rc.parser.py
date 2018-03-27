a66ed7826b7d9934181b0fcd270ea0561608c497
Rene Kjellerup <rk.katana.steel@gmail.com>
2018-03-27 16:07:28 -0700

    made rc parser direct include files it can reach.
    also made include path relative to the RC file.

--- libs/DynRes/rc.parser.py
+++ libs/DynRes/rc.parser.py
@@ -5,35 +5,70 @@ from sys import argv
 includes = []
 str_table = []
 text = []
-with open(argv[1], 'r') as f:
-    cur_lst = None
-    for l in f.readlines():
-        if '#include' in l.lower():
-            n = l.split('"')
-            includes.append(n[1].replace('\\', '/'))
-        if 'stringtable' in l.lower():
-            cur_lst = str_table
-        if 'end' in l.lower():
-            cur_lst = None
-        if l.startswith(' ') and cur_lst is not None:
-            n = l.split('"')
-            cur_lst.append((n[0].strip(), n[1]))
+defs = []
+
+
+def parseRC(rcFile):
+    with open(argv[1], 'r') as f:
+        cur_lst = None
+        str_id = None
+        txt = None
+        for l in f.readlines():
+            if l.lower().startswith('#include'):
+                n = l.split('"')
+                includes.append(n[1].replace('\\', '/'))
+            if 'stringtable' in l.lower():
+                cur_lst = str_table
+            if 'end' in l.lower():
+                cur_lst = None
+            if l.startswith(' ') and cur_lst is not None:
+                n = l.strip('\n').split('"')
+                if str_id is None:
+                    str_id = n[0].strip()
+                if len(n) is 1:
+                    continue
+                txt = '"'.join(n[1:-1])
+                cur_lst.append((str_id, txt))
+                str_id = None
+                txt = None
+
+
+def parseH(hFile):
+    try:
+        with open(hFile, 'r') as h:
+            for l in h.readlines():
+                defs.append(l.strip('\n'))
+    except Exception:
+        pass
+
+
+parseRC(argv[1])
+
+base = '/'.join(argv[1].split('/')[:-1])
+# fetching includes and dropping them directly into the generated file
+for i in includes[:]:
+    if '.h' in i:
+        print('including: {0}'.format(base + '/' + i))
+        parseH(base + '/' + i)
+    if '.rc' in i.lower():
+        print('including: {0}'.format(base + '/' + i))
+        parseRC(base + '/' + i)
 
 
 with open(argv[1].split('/')[-1] + '.cpp', 'w') as o:
-    o.write('''/* 
-  my template c++ file which implements the internal resource 
+    o.write('''/*
+  my template c++ file which implements the internal resource
   lookup and storage
 */
 
 #include "dynres.h"
-''')
-    for i in includes:
-        o.write('#include "{0}"'.format(i)+'\n')
-    o.write('''
 #include <string>
 #include <unordered_map>
 #include <cinttypes>
+''')
+    for i in defs:
+        o.write('{0}'.format(i)+'\n')
+    o.write('''
 
 static std::unordered_map<uint32_t, std::string> g_string_table;
 
