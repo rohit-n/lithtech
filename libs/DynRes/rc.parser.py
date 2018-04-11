29506e36bc264ba3fbb11c66e337971199d420e9
Rene Kjellerup <rk.katana.steel@gmail.com>
2018-04-11 14:56:19 -0700

    updated the resource compiler
    added include directories to the agrument list
    and include rc recursion...

--- libs/DynRes/rc.parser.py
+++ libs/DynRes/rc.parser.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
 from sys import argv
+from os.path import exists
 
-includes = []
 str_table = []
 text = []
 defs = []
 
 
 def parseRC(rcFile):
-    with open(argv[1], 'r') as f:
+    ret_incl = []
+    with open(rcFile, 'r') as f:
         cur_lst = None
         str_id = None
         txt = None
         for l in f.readlines():
             if l.lower().startswith('#include'):
                 n = l.split('"')
-                includes.append(n[1].replace('\\', '/'))
+                ret_incl.append(n[1].replace('\\', '/'))
             if 'stringtable' in l.lower():
                 cur_lst = str_table
             if 'end' in l.lower():
@@ -31,6 +32,7 @@ def parseRC(rcFile):
                 cur_lst.append((str_id, txt))
                 str_id = None
                 txt = None
+    return ret_incl
 
 
 def parseH(hFile):
@@ -42,20 +44,48 @@ def parseH(hFile):
         pass
 
 
-parseRC(argv[1])
+def getIncludes(base, paths, includes):
+    ret_incl = []
+    # fetching includes and dropping them directly into the generated file
+    for i in includes[:]:
+        inc = ''
+        for p in paths:
+            if exists('/'.join([base, p, i])):
+                inc = '/'.join([base, p, i])
+                break
+            if exists('/'.join([p, i])):
+                inc = '/'.join([p, i])
+                break
+        if exists('/'.join([base, i])):
+            inc = '/'.join([base, i])
+        if len(inc) < 1:
+            print('not found: {0}'.format(i))
+            continue
+        if '.h' in i.lower():
+            print('including: {0}'.format(inc))
+            parseH(inc)
+        if '.rc' in i.lower():
+            print('including: {0}'.format(inc))
+            ret_incl.extend(parseRC(inc))
+    return ret_incl
 
-base = '/'.join(argv[1].split('/')[:-1])
-# fetching includes and dropping them directly into the generated file
-for i in includes[:]:
-    if '.h' in i:
-        print('including: {0}'.format(base + '/' + i))
-        parseH(base + '/' + i)
-    if '.rc' in i.lower():
-        print('including: {0}'.format(base + '/' + i))
-        parseRC(base + '/' + i)
+
+main_rc = ''
+paths = ['.']
+for p in argv:
+    if p.lower().endswith('rc'):
+        main_rc = p
+        continue
+    paths.append(p)
+
+base = '/'.join(main_rc.split('/')[:-1])
+
+includes = parseRC(main_rc)
+while len(includes) > 0:
+    includes = getIncludes(base, paths, includes[:])
 
 
-with open(argv[1].split('/')[-1] + '.cpp', 'w') as o:
+with open(main_rc.split('/')[-1] + '.cpp', 'w') as o:
     o.write('''/*
   my template c++ file which implements the internal resource
   lookup and storage
@@ -75,9 +105,8 @@ with open(argv[1].split('/')[-1] + '.cpp', 'w') as o:
 std::vector<std::pair<uint32_t,const char*>> str_tab {
 ''')
     for x in str_table:
-        o.write('   std::make_pair({0},"{1}"),'.format(x[0], x[1]))
-    o.write('''
-    std::make_pair(0,"LithTech")
+        o.write('    std::make_pair({0},"{1}"),\n'.format(x[0], x[1]))
+    o.write('''    std::make_pair(0,"LithTech")
 };
 
 extern "C" {
