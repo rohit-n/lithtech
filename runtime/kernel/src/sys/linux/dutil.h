3611dc6f6da986cd56022b7011915a65cd7fd42e
Rene Kjellerup <rk.katana.steel@gmail.com>
2019-11-01 19:41:37 -0700

    removed dependency on the windows dutil definitions

--- runtime/kernel/src/sys/linux/dutil.h
+++ runtime/kernel/src/sys/linux/dutil.h
@@ -1,1 +1,11 @@
-#include "../win/dutil.h"
+// d_util defines lots of useful helper functions.
+#ifndef __DUTIL_H__
+#define __DUTIL_H__
+
+#ifndef __LTBASETYPES_H__
+#include "ltbasetypes.h"
+#endif
+
+bool du_UpperStrcmp(const char *pStr1, const char *pStr2);
+
+#endif  // __DUTIL_H__
