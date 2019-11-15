daf5e7f07e3a4c91cdd4b604c1f27401f0278db6
Rene Kjellerup <rk.katana.steel@gmail.com>
2019-11-15 13:04:32 -0800

    sorta implemented a basic ref counted SDL2 texture
    by default will make a 16x16 texture.

--- runtime/render_b/src/sys/sdl/sdltexinterface.cpp
+++ runtime/render_b/src/sys/sdl/sdltexinterface.cpp
@@ -7,29 +7,45 @@
 #include <SDL2/SDL_image.h>
 
 // interface database
-define_interface(CSysTexInterface, ILTTexInterface); 
+define_interface(CSysTexInterface, ILTTexInterface);
 
 // SharedTexture == SDL_Surface
+struct SharedTexture
+{
+    SDL_Surface *tex;
+    uint64_t ref;
 
+    SharedTexture(uint32 w=16, uint32 h=16) : ref(0)
+    {
+        tex = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
+    }
+
+    ~SharedTexture()
+    {
+        SDL_FreeSurface(tex);
+        tex = nullptr; // just incase
+    }
+};
 // Find texture in memory if it exists and return its handle.
 // If it does not exist, this function should return an error.
 LTRESULT CSysTexInterface::FindTextureFromName(HTEXTURE &hTexture, const char *pFilename) 
 {
-    hTexture = nullptr;
-    return LT_ERROR;
+    hTexture = new SharedTexture;
+    return LT_OK;
 }
 
 LTRESULT CSysTexInterface::CreateTextureFromName(HTEXTURE &hTexture, const char *pFilename)
 {
-    hTexture = nullptr;
-    return LT_ERROR;
+    hTexture = new SharedTexture;
+    return LT_OK;
 }
 
 LTRESULT CSysTexInterface::CreateTextureFromData(HTEXTURE &hTexture, ETextureType eTextureType, uint32 TextureFlags, 
 	uint8 *pData, uint32 nWidth, uint32 nHeight, uint32 nAutoGenMipMaps) 
 {
-    hTexture = nullptr;
-    return LT_ERROR;
+    hTexture = new SharedTexture(nWidth, nHeight);
+    hTexture->tex->pixels = pData;
+    return LT_OK;
 }
 
 LTRESULT CSysTexInterface::GetTextureData(const HTEXTURE hTexture, const uint8* &pData, uint32 &nPitch, uint32& nWidth, uint32& nHeight, ETextureType& eType) 
@@ -50,15 +66,26 @@ LTRESULT CSysTexInterface::GetTextureType(const HTEXTURE hTexture, ETextureType 
 
 LTRESULT CSysTexInterface::GetTextureDims(const HTEXTURE hTexture, uint32 &nWidth, uint32 &nHeight) 
 {
-    return LT_ERROR;
+    if (!hTexture)
+        return LT_ERROR;
+    nWidth = hTexture->tex->w;
+    nHeight = hTexture->tex->h;
+    return LT_OK;
 }
 
 bool CSysTexInterface::ReleaseTextureHandle(const HTEXTURE hTexture)
 {
+    if(hTexture)
+        if (--(hTexture->ref) < 1) {
+            delete hTexture;
+            return true;
+        }
     return false;
 }
 
 uint32 CSysTexInterface::AddRefTextureHandle(const HTEXTURE hTexture)
 {
+    if(hTexture)
+        return ++hTexture->ref;
     return 0;
 }
