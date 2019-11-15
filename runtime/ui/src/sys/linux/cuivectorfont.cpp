310c1edccac48e770af8b9a011e00a1ca8699a42
Rene Kjellerup <rk.katana.steel@gmail.com>
2019-11-15 13:08:28 -0800

    made the most minimal viable Vector Font "implementation"
    it will accept any font face without actually loading anything

--- runtime/ui/src/sys/linux/cuivectorfont.cpp
+++ runtime/ui/src/sys/linux/cuivectorfont.cpp
@@ -34,28 +34,51 @@ class InstalledFontFace
 {
 };
 
-bool CUIVectorFont::CreateFontTextureAndTable(InstalledFontFace&,char const*,bool)
+bool CUIVectorFont::CreateFontTextureAndTable(InstalledFontFace&,char const*,bool bMakeMap)
 {
-    // RKJ Stub return success alway until we start implementing it
+    // this is an elaborate stub like a null font, defined for every character.
+    Term();
+    auto nLen = 256;
+    LT_MEM_TRACK_ALLOC(m_pFontTable = new uint16[nLen * 3],LT_MEM_TYPE_UI);
+    memset(m_pFontTable, 0, sizeof(uint16)*nLen*3);
+    if (bMakeMap)
+    {
+        LT_MEM_TRACK_ALLOC(m_pFontMap = new uint8[nLen],LT_MEM_TYPE_UI);
+        m_bAllocatedMap = ( m_pFontMap != nullptr );
+    }
+
+    m_Texture = nullptr;
+    m_Proportional = true;
+    m_Valid = true;
+
     return true;
 }
 
 bool
-CUIVectorFont::Init(const char *, const char*, uint32, uint8, uint8,LTFontParams*)
+CUIVectorFont::Init(const char *, const char* face, uint32 pointSize, uint8, uint8,LTFontParams*)
 {
     // RKJ Stub return success alway until we start implementing it
-    return true;
+    m_PointSize = pointSize;
+    InstalledFontFace font;
+    return CreateFontTextureAndTable(font, face, true);
 }
 
 bool
-CUIVectorFont::Init(const char *, const char*,uint32,const char *,LTFontParams*)
+CUIVectorFont::Init(const char *, const char* face,uint32 pointsize,const char *,LTFontParams*)
 {
     // RKJ Stub return success alway until we start implementing it
-    return true;
+    return Init("", face, pointsize, 0, 0, nullptr);
 }
 
 void CUIVectorFont::Term()
 {
+    if(m_pFontMap)
+        delete[] m_pFontMap;
+    if(m_pFontTable)
+        delete[] m_pFontTable;
+
+    m_pFontMap = nullptr;
+    m_pFontTable = nullptr;
     // RKJ Stub
 }
 
