af3373e95c87160bd362b02ee7e821eb46e46a31
Rohit Nirmal <rohitnirmal9@gmail.com>
2021-05-20 16:40:52 -0500

    Undo a bunch of old offsetting, and replace it with the new way! Taken from https://github.com/haekb/nolf2-modernizer/commit/da07ebb260adb6c6c24459a6a6ed5b0161ff0398

--- NOLF2/ClientShellDLL/TO2/ScreenPerformance.cpp
+++ NOLF2/ClientShellDLL/TO2/ScreenPerformance.cpp
@@ -312,7 +312,7 @@ LTBOOL CScreenPerformance::Build()
 
 	m_pDlg = debug_new(CLTGUIWindow);
 	m_pDlg->Create(g_pInterfaceResMgr->GetTexture(szBack), kDlgWd, kDlgHt);
-	dlgPos.x += g_pInterfaceResMgr->Get640x480Offset();
+
 	m_pDlg->SetBasePos(dlgPos);
 	m_pDlg->ApplyPosition(g_pInterfaceResMgr->GetYRatio(), g_pInterfaceResMgr->Get4x3Offset());
 
